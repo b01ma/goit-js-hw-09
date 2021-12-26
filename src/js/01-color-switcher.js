1251f768382d3a6c4e9ea93e6d690733aa68445d
ed_bolma <ed.bolma21@gmail.com>
2021-12-26 16:57:58 +0100

    task 1 is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onClickStart);
+refs.stopBtn.addEventListener('click', onClickStop);
+
+let timerID = null;
+
+function onClickStart (event) {
+    // console.log(event.target);
+    timerID = setInterval(changeBgColor, 500);
+
+    refs.startBtn.disabled = true;
+    // refs.stopBtn.disabled = false;
+};
+function onClickStop (event) {
+    // console.log(event.target);
+    clearInterval(timerID);
+    // refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBgColor() {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+
+}
